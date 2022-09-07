eac1d2364adb959dfa84727308d72894efd53640
stevekineeve88 <stevemamajama@gmail.com>
2022-09-06 20:30:20 -0400

    added alpha package for mysql data manager

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     url="https://github.com/stevekineeve88/mysql_schema_manager_library.git",
     packages=setuptools.find_packages(),
     install_requires=[
-        "mysql-data-manager @ git+https://github.com/stevekineeve88/mysql_data_manager_library@VERSION-0.0.1#egg=mysql-data-manager"
+        "mysql-data-manager @ git+https://github.com/stevekineeve88/mysql_data_manager_library@v0.0.1-alpha#egg=mysql-data-manager"
     ],
     python_requires='>=3.7'
 )
